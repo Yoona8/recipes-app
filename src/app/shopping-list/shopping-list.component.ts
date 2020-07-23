fc75847b10bc7d86cac0210fb8bf320507b8514d
Yoona8 <elena243v@gmail.com>
2020-07-23 09:59:59 +0300

    Fill shoping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+// @ts-ignore
+import * as ingredientsData from '../../mocks/ingredients.json';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ShoppingListComponent {
+  public ingredients: Ingredient[] = ingredientsData.default;
 }
