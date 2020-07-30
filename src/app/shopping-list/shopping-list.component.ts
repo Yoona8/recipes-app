d812438455aeb33c1ff21ee1317b295b262662d7
Yoona8 <elena243v@gmail.com>
2020-07-30 09:22:05 +0300

    Refactor shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   private _ingredients: Ingredient[];
+  private _ingredientsChanged$: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this._ingredients = this.shoppingListService.ingredients;
-    this.shoppingListService.ingredientsChanged
+    this._ingredientsChanged$ = this.shoppingListService.ingredientsChanged$
       .subscribe((ingredients: Ingredient[]) => {
         this._ingredients = ingredients;
       });
+  }
+
+  ngOnDestroy(): void {
+    this._ingredientsChanged$.unsubscribe();
   }
 
   get ingredients(): Ingredient[] {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,28 +1,25 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Ingredient } from '../shared/ingredient.model';
 // @ts-ignore
 import * as ingredientsData from '../../mocks/ingredients.json';
-import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({providedIn: 'root'})
 export class ShoppingListService {
   private _ingredients: Ingredient[] = ingredientsData.default;
-  private _ingredientsChanged = new EventEmitter<Ingredient[]>();
+  public ingredientsChanged$ = new Subject<Ingredient[]>();
 
   get ingredients(): Ingredient[] {
     return this._ingredients.slice();
   }
 
-  get ingredientsChanged(): EventEmitter<Ingredient[]> {
-    return this._ingredientsChanged;
-  }
-
   addIngredient(ingredient: Ingredient) {
     this._ingredients.push(ingredient);
-    this._ingredientsChanged.emit(this._ingredients.slice());
+    this.ingredientsChanged$.next(this._ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this._ingredients.push(...ingredients);
-    this._ingredientsChanged.emit(this._ingredients.slice());
+    this.ingredientsChanged$.next(this._ingredients.slice());
   }
 }
